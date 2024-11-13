dc0d7ec0865fa8c73054bacf8ec5252eef60c7fb
Daniele Veglia <callmenoth@gmail.com>
2024-11-13 11:19:03 -0500

    Update controllo nome citta'

--- Unit1/Settimana 2/S2L3/Esercizio.py
+++ Unit1/Settimana 2/S2L3/Esercizio.py
@@ -1,13 +1,13 @@
 
 
 city = input("Inserisci la tua città: ")
-while not city.isalpha() or city.strip() == "":                                 # Controllo se il nome contiene solo lettere e non sia una stringa vuota
-    print("Per favore, inserisci un nome di citta' valido(Usa solo lettere)")
+while not all(c.isalpha() or c.isspace() for c in city) or city.strip() == "":     # Controllo se il nome contiene solo lettere e non sia una stringa vuota
+    print("Per favore, inserisci un nome di citta' valido(Usa solo lettere).")
     city = input("Inserisci la tua città: ")
 
 animal = input("Inserisci il nome del tuo animale: ")
-while animal.strip() == "":                                                     #  Controllo se il nome non sia una stringa vuota
-    print("Per favore, inserisci il nome del tuo animale")
+while animal.strip() == "":                                                         #  Controllo se il nome non sia una stringa vuota
+    print("Per favore, inserisci il nome del tuo animale.")
     animal = input("Inserisci il nome del tuo animale: ")
 
 
