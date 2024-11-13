0facbb83bdeaa15380fe24540556624d4e2eb79a
Daniele Veglia <callmenoth@gmail.com>
2024-11-13 11:10:45 -0500

    [Update] Aggiunti commenti

--- Unit1/Settimana 2/S2L3/Esercizio.py
+++ Unit1/Settimana 2/S2L3/Esercizio.py
@@ -1,16 +1,16 @@
 
 
 city = input("Inserisci la tua città: ")
-while not city.isalpha() or city.strip() == "":
+while not city.isalpha() or city.strip() == "":                                 # Controllo se il nome contiene solo lettere e non sia una stringa vuota
     print("Per favore, inserisci un nome di citta' valido(Usa solo lettere)")
     city = input("Inserisci la tua città: ")
 
 animal = input("Inserisci il nome del tuo animale: ")
-while animal.strip() == "":
+while animal.strip() == "":                                                     #  Controllo se il nome non sia una stringa vuota
     print("Per favore, inserisci il nome del tuo animale")
     animal = input("Inserisci il nome del tuo animale: ")
 
-    
+
 band_name = city + " " + animal
 
 
