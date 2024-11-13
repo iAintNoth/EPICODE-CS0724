df19c1b83746ec0a139726cead6887b25deab3cc
Daniele Veglia <callmenoth@gmail.com>
2024-11-13 09:00:39 -0500

    Up

---
+++ Unit1/Settimana 2/S2L3/Esercizio.py
@@ -0,0 +1,12 @@
+
+
+city = input("Inserisci la tua città: ")
+
+animal = input("Inserisci il nome del tuo animale: ")
+
+
+band_name = city + " " + animal
+
+
+print("Il nome della band e':", band_name)
+
