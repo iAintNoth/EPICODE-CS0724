e33e4b4401c2bf19b6bb523be0e756ea270ca34d
Daniele Veglia <callmenoth@gmail.com>
2024-11-13 11:09:27 -0500

    Update codice

--- Unit1/Settimana 2/S2L3/Esercizio.py
+++ Unit1/Settimana 2/S2L3/Esercizio.py
@@ -1,11 +1,18 @@
 
 
 city = input("Inserisci la tua città: ")
+while not city.isalpha() or city.strip() == "":
+    print("Per favore, inserisci un nome di citta' valido(Usa solo lettere)")
+    city = input("Inserisci la tua città: ")
 
 animal = input("Inserisci il nome del tuo animale: ")
+while animal.strip() == "":
+    print("Per favore, inserisci il nome del tuo animale")
+    animal = input("Inserisci il nome del tuo animale: ")
 
-
+    
 band_name = city + " " + animal
+
 
 
 print("Il nome della band e':", band_name)
